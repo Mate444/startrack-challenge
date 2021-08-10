665975382f75f8c70f2b6e619360ef74e0079df5
Mateo Hernandez <73136132+Mate444@users.noreply.github.com>
2021-08-10 14:18:30 -0300

    adds lazy-loading to heros (requires improvement)

--- client/src/components/General/General.tsx
+++ client/src/components/General/General.tsx
@@ -5,30 +5,47 @@ import { GeneralProps, HeroType } from "../../types";
 const General = (props: GeneralProps) => {
   const { heroes, setHeroes } = props;
   const [ lastIndex, setLastIndex ] = useState(16);
-  const loader = useRef(null);
-    console.log();
-  const handleObserver = useCallback((entries) => {
-    const target = entries[0];
-    console.log(target)
-    if (target.isIntersecting) {
-      setLastIndex(lastIndex + 16);
-    }
-  }, [setLastIndex, lastIndex])
+  // const loader = useRef(null);
+  //   console.log();
+  // const handleObserver = useCallback((entries) => {
+  //   const target = entries[0];
+  //   console.log(target)
+  //   //si el target esta en la pagina ( a la vista)
+  //   if (target.isIntersecting) {
+  //     setLastIndex(lastIndex + 16);
+  //   }
+  // }, [setLastIndex, lastIndex])
   
-  useEffect(() => {
-    const target = document.querySelector('#heroes-list');
-    const options = {
-      root: target,
-      rootMargin: '0px',
-      threshold: 0.5
-    };
-    const observer = new IntersectionObserver(handleObserver, options);
-    console.log(loader)
-    if (loader && loader.current) {
-      target && observer.observe(target);
-    }
-  }, [loader, handleObserver])
-  console.log(lastIndex)
+  // useEffect(() => {
+  //   const target = document.querySelector('heroes-window');
+  //   const options = {
+  //     root: target,
+  //     rootMargin: '0px',
+  //     threshold: 0.5
+  //   };
+  //   const observer = new IntersectionObserver(handleObserver, options);
+  //   console.log(loader)
+  //   if (loader && loader.current) {
+  //     target && observer.observe(target);
+  //   }
+  // }, [loader, handleObserver])
+  // console.log(lastIndex)
+  const target = document.querySelector('#general-loader');
+  console.log(target)
+  const options = {
+    root: null, //this is the viewport
+    threshold: 0.50, // esto seria cuanto porcentaje del target tiene que verse para que se ejecute el observer
+    rootMargin: '150px' //Esto hace que se demore mas el viewport en ser watcheado
+  };
+  const observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach((e) => {
+      console.log(e)
+      if (e.isIntersecting) {
+        setLastIndex(lastIndex + 8);
+      }
+    })
+  }, options)
+  target && observer.observe(target)
   useEffect(() => {
     async function getHeroes() {
       axios
@@ -52,11 +69,10 @@ const General = (props: GeneralProps) => {
     getHeroes();
     //eslint-disable-next-line
   }, []);
-  console.log(heroes);
   const heroesToDisplay = heroes?.slice(0, lastIndex);
   return <div>
     <SearchBar />
-    <div ref={loader} id='heroes-list' className='heroes-window'>
+    <div className='heroes-window'>
     {
       heroes && heroesToDisplay?.map((h, i) => (
         <div key={i}>
@@ -66,6 +82,7 @@ const General = (props: GeneralProps) => {
         </div>
       ))
     }
+    <p id='general-loader'>Loading...</p>
     </div>    
   </div>;
 };
