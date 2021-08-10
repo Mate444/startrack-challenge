314e7cad174d6d9f65ccda15e57f62dceba9de76
Mateo Hernandez <73136132+Mate444@users.noreply.github.com>
2021-08-10 12:39:23 -0300

    adds collapsable navbar to favorite heroes

--- client/src/components/Favorites/Favorites.tsx
+++ client/src/components/Favorites/Favorites.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FavoriteProps, HeroType } from '../../types';
 import FavoriteHero from '../FavoriteHero/FavoriteHero';
 
@@ -6,7 +6,6 @@ function filterProperly(arr1:any, arr2:any) {
   const newArr = [];
   for (let i = 0; i < arr1.length; i++) {
     if (arr2.indexOf(arr1[i].id) !== - 1) {
-      console.log(newArr)
       newArr.push(arr1[i])
     }
   }
@@ -14,24 +13,39 @@ function filterProperly(arr1:any, arr2:any) {
 }
 
 const Favorites = (props: FavoriteProps) => {
-    const { heroes, favoriteHeroes, setFavoriteHeroes } = props;
-function handleFavoriteHeroes(id: number) {
-  const filteredFavoriteHeroes = favoriteHeroes.filter((heroId: number) => heroId !== id);
-  localStorage.setItem('favoritesArray', JSON.stringify(filteredFavoriteHeroes));
-  setFavoriteHeroes(filteredFavoriteHeroes);
-}
-console.log(favoriteHeroes)
-const filteredFavorites = heroes?.filter((h) => favoriteHeroes.indexOf(h.id) !== -1);
-console.log(filteredFavorites)
+  const { heroes, favoriteHeroes, setFavoriteHeroes } = props;
+  const [toggle, setToggle] = useState('open');
+  function handleFavoriteHeroes(id: number) {
+    const filteredFavoriteHeroes = favoriteHeroes.filter((heroId: number) => heroId !== id);
+    localStorage.setItem('favoritesArray', JSON.stringify(filteredFavoriteHeroes));
+    setFavoriteHeroes(filteredFavoriteHeroes);
+  };
+  const filteredFavorites = heroes?.filter((h) => favoriteHeroes.indexOf(h.id) !== -1);
   return (
     <div>
       {
-        filteredFavorites && filteredFavorites.map((h: HeroType, i: number) => (
-          <div key={i}>
-            <FavoriteHero hero={h} handleFavoriteHeroes={handleFavoriteHeroes} favoriteHeroes={favoriteHeroes} />
+        toggle === 'open' &&
+        <div>
+          <div>
+          {
+          filteredFavorites && filteredFavorites.map((h: HeroType, i: number) => (
+            <div key={i}>
+                <FavoriteHero hero={h} handleFavoriteHeroes={handleFavoriteHeroes} favoriteHeroes={favoriteHeroes} />
+              </div>
+            ))
+         }
+         <button onClick={() => setToggle('closed')}>▲</button>
           </div>
-        ))
+        </div>
       }
+      {
+        toggle === 'closed' &&
+        <div>
+          <p>Liked</p>
+          <button onClick={() => setToggle('open')}>▼</button>
+        </div>
+      }
+  
     </div>
   );
 };
